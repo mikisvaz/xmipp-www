6bb7fed536b7f24b7eb18368ea017034e2712573
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-03-05 18:49:02 +0100

    Clean option hash. Just in case...

--- lib/xmipp.rb
+++ lib/xmipp.rb
@@ -16,10 +16,30 @@ module XMIPP
     "sampling_rate" =>1,
   }
 
-  def self.volume_to_pseudoatom(infile, outfile, options = {})
-    options = Hash[*options.collect{|key, value| [key.to_s, value]}.flatten]
+  STRING_OPTIONS = { "intensityColumn" => %w(occupancy Bfactor) }
+
+  def self.process_options(options)
+    options.delete_if{|key, value| ! DEFAULT_OPTIONS.keys.include?(key.to_s)}
+    options = Hash[*options.collect{|key, value| 
+      key = key.to_s
+      case
+      when Integer === DEFAULT_OPTIONS[key] || Float === DEFAULT_OPTIONS[key]
+        value = value.to_f
+      when FalseClass === DEFAULT_OPTIONS[key] || TrueClass === DEFAULT_OPTIONS[key]
+        value = value == true
+      else 
+        value = STRING_OPTIONS[key].include?(value) ? value : DEFAULT_OPTIONS[key] 
+      end
+      [key, value]
+    }.flatten]
     options = DEFAULT_OPTIONS.merge(options)
-    options.delete_if{|key, value| ! DEFAULT_OPTIONS.keys.include?(key) || value == false}
+    options.delete_if{|key, value| ! value}
+    options
+  end
+
+  def self.volume_to_pseudoatom(infile, outfile, options = {})
+    options = process_options(options)
+    p options
 
     params  = options.collect {|param, value| "-#{param} #{value}" } * " "
     cmd = "#{File.join(BIN_DIR, 'xmipp_convert_vol2pseudo')} #{params} -i #{infile} -o #{outfile} -thr 2"
@@ -28,5 +48,5 @@ module XMIPP
 end
 
 if __FILE__ == $0
-  XMIPP.volume_to_pseudoatom('test/data/PolAB_msk4.spi', 'tmp/PolAB_msk4')
+  XMIPP.volume_to_pseudoatom('test/data/PolAB_msk4.spi', 'tmp/PolAB_msk4', {})
 end
